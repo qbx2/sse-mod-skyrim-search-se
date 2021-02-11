89b6cd553a36e4827bb883fe068be901413cc92d
qbx2 <sunyeop97@gmail.com>
2021-02-12 01:52:50 +0900

    Remove TESNPC.get_edid hook
    Although this hook fixes the empty-edid problem of 'help command',
    it prevents the user from finding NPC by its name. (SkyrimSE.exe+0x312164)

--- src/form/npc.rs
+++ src/form/npc.rs
@@ -8,7 +8,6 @@ use crate::db;
 use rusqlite::params;
 
 static_detour! {
-    static NpcEdidGetter: fn(*const c_void) -> *const c_char;
     static NpcEdidSetter: fn(*const c_void, *const c_char) -> bool;
 }
 
@@ -20,20 +19,6 @@ struct TESForm {
     form_id: u32,
     unknown_18: u16,
     form_type: u8,
-}
-
-fn new_edid_getter(this: &TESForm) -> *const c_char {
-    let result: Result<*const c_char, anyhow::Error> = try {
-        let db = db::DB.lock().unwrap();
-        let mut stmt = db.prepare("SELECT edid FROM forms WHERE id = ?")?;
-        let mut edid_iter = stmt.query_map(&[this.form_id], |row| {
-            Ok(row.get(0)?)
-        })?;
-        let result: String = edid_iter.next().ok_or(anyhow::anyhow!("not found"))??;
-        output_debug_string(format!("override {:#x} edid -> {}", this.form_id, result).as_str());
-        CString::new(result)?.into_raw() as *const c_char
-    };
-    result.unwrap_or(0 as *const c_char)
 }
 
 fn new_edid_setter(this: &TESForm, edid: *const c_char) -> bool {
@@ -58,9 +43,8 @@ fn new_edid_setter(this: &TESForm, edid: *const c_char) -> bool {
 pub(crate) unsafe fn init(image_base: usize) -> anyhow::Result<()> {
     let npc_vtable = image_base + 0x159fcd0;
 
-    output_debug_string(format!("getter: {:#x}, setter: {:#x}", npc_vtable + 0x190, npc_vtable + 0x198).as_str());
+    output_debug_string(format!("setter: {:#x}", npc_vtable + 0x198).as_str());
 
-    patch_bytes(&(new_edid_getter as usize), (npc_vtable + 0x190) as *mut c_void, 8)?;
     patch_bytes(&(new_edid_setter as usize), (npc_vtable + 0x198) as *mut c_void, 8)?;
 
     Ok(())
